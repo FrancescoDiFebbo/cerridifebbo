17a77c3adacc9c9d69ade7204749d65e21b1b611
Francesco <Francesco@Francesco>
2015-04-21 19:10:19 +0200

    #1

---
+++ cerridifebbo/src/main/java/it/polimi/ingsw/cerridifebbo/App.java
@@ -0,0 +1,13 @@
+package it.polimi.ingsw.cerridifebbo;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
