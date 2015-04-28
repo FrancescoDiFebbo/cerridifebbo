49d72acdb47afe2475f87d62ff2c8ada62426a42
StefanoCerri <stefano1.cerri@mail.polimi.it>
2015-04-28 16:48:53 +0200

    # update model package

---
+++ cerridifebbo/src/main/java/it/polimi/ingsw/cerridifebbo/model/ConcreteDeckFactory.java
@@ -0,0 +1,5 @@
+package it.polimi.ingsw.cerridifebbo.model;
+
+public class ConcreteDeckFactory implements DeckFactory{
+
+}
---
+++ cerridifebbo/src/main/java/it/polimi/ingsw/cerridifebbo/model/Deck.java
@@ -0,0 +1,17 @@
+package it.polimi.ingsw.cerridifebbo.model;
+
+public class Deck {
+	private String name;
+	private Card [] cards;
+	
+	public void draw ()
+	{
+		
+	}
+	
+	public void discardCard ()
+	{
+		
+	}
+
+}
