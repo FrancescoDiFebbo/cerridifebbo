00ff60ac9b28f7420a3834c3ef514eff693979ae
FrancescoDiFebbo <francesco.difebbo@mail.polimi.it>
2015-05-11 19:07:15 +0200

    modifica settori adiacenti

--- cerridifebbo/src/main/java/it/polimi/ingsw/cerridifebbo/model/Map.java
+++ cerridifebbo/src/main/java/it/polimi/ingsw/cerridifebbo/model/Map.java
@@ -1,56 +1,93 @@
 package it.polimi.ingsw.cerridifebbo.model;
-
 import java.io.File;
 import java.io.IOException;
 import java.util.Scanner;
 
 public class Map {
-	
+
 	private String name;
 	public static final int COLUMNMAP = 23;
 	public static final int RAWMAP = 14;
-	
-	
-	private Sector[][] grid = new Sector [RAWMAP][COLUMNMAP];
-	
-	
+
+	private Sector[][] grid = new Sector[RAWMAP][COLUMNMAP];
+
 	public void createMap(File mapFile) {
-		
-		try{
+
+		try {
 			SectorFactory factory = new ConcreteSectorFactory();
 			Scanner fr = new Scanner(mapFile);
-			name= mapFile.getName();
-			for(int i=0; i<RAWMAP; i++ )
-			{
-				for (int j=0; j<COLUMNMAP; j++ )
-				{
-					grid[i][j] = factory.createSector(fr.nextInt(),i ,j);
+			name = mapFile.getName();
+			for (int i = 0; i < RAWMAP; i++) {
+				for (int j = 0; j < COLUMNMAP; j++) {
+					grid[i][j] = factory.createSector(fr.nextInt(), i, j);
 				}
 			}
 			fr.close();
-			for(int i=0; i<RAWMAP; i++ )
-			{
-				for (int j=0; j<COLUMNMAP; j++ )
-				{
-					grid[i][j].setAdjacentSector();
+			for (int j = 0; j < COLUMNMAP; j++) {
+				for (int i = 0; i < RAWMAP; i++) {
+					setAdjacentSector(i, j);
 				}
 			}
-		}
-		catch (IOException e)
-		{
-		    e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
 		}
 	}
-	
-	
-	public  static void main(String[] args)
-	{
-		File a = new File ("C://Users//stefano//git//cerridifebbo//cerridifebbo//map//galilei.txt");
+
+	private void setAdjacentSector(int raw, int column) {
+		setNorthEast(raw, column);
+		setSouthEast(raw, column);
+		setSouth(raw, column);
+	}
+
+	private void setNorthEast(int raw, int column) {
+		if (column % 2 == 0) {
+			if (raw - 1 > 0 && column + 1 < COLUMNMAP) {
+				grid[raw][column].setNorthEast(grid[raw - 1][column + 1]);
+				grid[raw - 1][column + 1].setSouthWest(grid[raw][column]);
+			} else {
+				grid[raw][column].setNorthEast(null);
+			}
+		} else {
+			if (column + 1 < COLUMNMAP) {
+				grid[raw][column].setNorthEast(grid[raw][column + 1]);
+				grid[raw][column + 1].setSouthWest(grid[raw][column]);
+			} else {
+				grid[raw][column].setNorthEast(null);
+			}
+		}
+	}
+
+	private void setSouthEast(int raw, int column) {
+		if (column % 2 == 0) {
+			if (column + 1 < COLUMNMAP) {
+				grid[raw][column].setSouthEast(grid[raw][column + 1]);
+				grid[raw][column + 1].setNorthWest(grid[raw][column]);
+			} else {
+				grid[raw][column].setSouthEast(null);
+			}
+		} else {
+			if (column + 1 < COLUMNMAP && raw + 1 < RAWMAP) {
+				grid[raw][column].setSouthEast(grid[raw + 1][column + 1]);
+				grid[raw + 1][column + 1].setNorthWest(grid[raw][column]);
+			} else {
+				grid[raw][column].setSouthEast(null);
+			}
+		}
+	}
+
+	private void setSouth(int raw, int column) {
+		if (raw + 1 < RAWMAP) {
+			grid[raw][column].setSouth(grid[raw + 1][column]);
+			grid[raw + 1][column].setNorth(grid[raw][column]);
+		} else {
+			grid[raw][column].setSouth(null);
+		}
+	}
+
+	public static void main(String[] args) {
+		File a = new File("C://Users//stefano//git//cerridifebbo//cerridifebbo//map//galilei.txt");
 		Map ao = new Map();
 		ao.createMap(a);
-		
 	}
-	
-	
-	
+
 }
--- cerridifebbo/src/main/java/it/polimi/ingsw/cerridifebbo/model/Sector.java
+++ cerridifebbo/src/main/java/it/polimi/ingsw/cerridifebbo/model/Sector.java
@@ -1,112 +1,45 @@
 package it.polimi.ingsw.cerridifebbo.model;
-
 public class Sector {
 	private Coordinate coordinate;
 	private boolean passable;
 	private Player[] playerInside;
-	
+
 	private Sector north;
 	private Sector south;
-	private Sector northEst;
-	private Sector southEst;
+	private Sector northEast;
+	private Sector southEast;
 	private Sector northWest;
 	private Sector southWest;
-	
-	public Sector (int raw, int column, boolean passable){
-		coordinate = new Coordinate (raw, column);
+
+	public Sector(int raw, int column, boolean passable) {
+		coordinate = new Coordinate(raw, column);
 		setPassable(passable);
 	}
-	
-	public void setAdjacentSector ()
-	{
-		
-		setNorth();
-		setSouth();
-		setEstWest(coordinate.getColumn()%2);
-		
-		
+
+	public void setNorthEast(Sector sector) {
+		this.northEast = sector;
 	}
 
-	private boolean checkOutOfBorders(int raw, int column)
-	{
-		if(raw < 0 || raw > Map.RAWMAP ||
-		   column <0 || column > Map.COLUMNMAP)
-			return false;
-		return true;
+	public void setNorthWest(Sector sector) {
+		this.northWest = sector;
 	}
-	
-	private void setNorth ()
-	{
-		if(checkOutOfBorders(coordinate.getRaw()+1,coordinate.getColumn()))
-		{
-			//north
-		}
-		else
-		{
-			north = null;
-		}
+
+	public void setSouthEast(Sector sector) {
+		this.southEast = sector;
 	}
-	
-	private void setSouth ()
-	{
-		if(checkOutOfBorders(coordinate.getRaw()-1,coordinate.getColumn()))
-		{
-			//south
-		}
-		else
-		{
-			south = null;
-		}
+
+	public void setSouthWest(Sector sector) {
+		this.southWest = sector;
 	}
-	
-	private void setEstWest(int type)
-	{
-		int shiftColumnNorth=0;
-		int shiftColumnSouth=0;
-		
-		switch (type)
-		{
-		case 0:
-			shiftColumnNorth=-1;
-		case 1:
-			shiftColumnSouth=+1;
-		}
-		
-		if(checkOutOfBorders(coordinate.getRaw()+shiftColumnNorth,coordinate.getColumn()+1))
-		{
-			//northEst
-		}
-		else
-		{
-			northEst = null;
-		}
-		if(checkOutOfBorders(coordinate.getRaw()+shiftColumnNorth,coordinate.getColumn()-1))
-		{
-			//northWest
-		}
-		else
-		{
-			northWest = null;
-		}
-		if(checkOutOfBorders(coordinate.getRaw()+shiftColumnSouth,coordinate.getColumn()+1))
-		{
-			//southEst
-		}
-		else
-		{
-			southEst = null;
-		}
-		if(checkOutOfBorders(coordinate.getRaw()+shiftColumnSouth,coordinate.getColumn()-1))
-		{
-			//southWest
-		}
-		else
-		{
-			southWest = null;
-		}
-	
+
+	public void setNorth(Sector sector) {
+		this.north = sector;
 	}
-	
+
+	public void setSouth(Sector sector) {
+		this.south = sector;
+	}
+
 	public void playerEnters(Player player) {
 
 	}
