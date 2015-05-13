9d4d61938cfb8dc4dcc1ecb9cfc8575ef51bf4e7
StefanoCerri <stefano1.cerri@mail.polimi.it>
2015-05-13 16:08:35 +0200

    #singleton mappa e alcune modifiche

--- cerridifebbo/src/main/java/it/polimi/ingsw/cerridifebbo/model/Map.java
+++ cerridifebbo/src/main/java/it/polimi/ingsw/cerridifebbo/model/Map.java
@@ -7,100 +7,20 @@ import java.util.Scanner;
 public class Map {
 
 	private String name;
-	public static final int COLUMNMAP = 23;
-	public static final int RAWMAP = 14;
+	private static Map instance=null;
+	private  final Sector[][] grid;
 
-	private Sector[][] grid = new Sector[RAWMAP][COLUMNMAP];
-
-	public void createMap(File mapFile) {
-
-		try {
-			SectorFactory factory = new ConcreteSectorFactory();
-			Scanner fr = new Scanner(mapFile);
-			name = mapFile.getName();
-			for (int i = 0; i < RAWMAP; i++) {
-				for (int j = 0; j < COLUMNMAP; j++) {
-					grid[i][j] = factory.createSector(fr.nextInt(), i, j);
-				}
-			}
-			fr.close();
-			for (int j = 0; j < COLUMNMAP; j++) {
-				for (int i = 0; i < RAWMAP; i++) {
-					if (grid[i][j] != null) {
-						setAdjacentSector(i, j);
-					}
-				}
-			}
-
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-
-		Player test = new Player();
-		test.setMaxMovement(3);
-		test.setPos(grid[5][5]);
-		System.out.println("val " + grid[7][9].isReachable(test));
-	}
-
-	private void setAdjacentSector(int raw, int column) {
-		setNorthEast(raw, column);
-		setSouthEast(raw, column);
-		setSouth(raw, column);
-	}
-
-	private void setNorthEast(int raw, int column) {
-		if (column % 2 == 0) {
-			if (raw - 1 >= 0 && column + 1 < COLUMNMAP
-					&& grid[raw - 1][column + 1] != null) {
-				grid[raw][column].setNorthEast(grid[raw - 1][column + 1]);
-				grid[raw - 1][column + 1].setSouthWest(grid[raw][column]);
-			} else {
-				grid[raw][column].setNorthEast(null);
-			}
-		} else {
-			if (column + 1 < COLUMNMAP && grid[raw][column + 1] != null) {
-				grid[raw][column].setNorthEast(grid[raw][column + 1]);
-				grid[raw][column + 1].setSouthWest(grid[raw][column]);
-			} else {
-				grid[raw][column].setNorthEast(null);
-			}
-		}
-	}
-
-	private void setSouthEast(int raw, int column) {
-		if (column % 2 == 0) {
-			if (column + 1 < COLUMNMAP && grid[raw][column + 1] != null) {
-				grid[raw][column].setSouthEast(grid[raw][column + 1]);
-				grid[raw][column + 1].setNorthWest(grid[raw][column]);
-			} else {
-				grid[raw][column].setSouthEast(null);
-			}
-		} else {
-			if (column + 1 < COLUMNMAP && raw + 1 < RAWMAP
-					&& grid[raw + 1][column + 1] != null) {
-				grid[raw][column].setSouthEast(grid[raw + 1][column + 1]);
-				grid[raw + 1][column + 1].setNorthWest(grid[raw][column]);
-			} else {
-				grid[raw][column].setSouthEast(null);
-			}
-		}
-	}
-
-	private void setSouth(int raw, int column) {
-		if (raw + 1 < RAWMAP && grid[raw + 1][column] != null) {
-			grid[raw][column].setSouth(grid[raw + 1][column]);
-			grid[raw + 1][column].setNorth(grid[raw][column]);
-		} else {
-			grid[raw][column].setSouth(null);
-		}
-	}
-
-	public static void main(String[] args) {
-		File a = new File(
-				"C://Users//stefano//git//cerridifebbo//cerridifebbo//map//galilei.txt");
-		Map ao = new Map();
-		ao.createMap(a);
+	public static Map getInstance() {
+		if (instance != null)
+			return instance;
+		File fileMap = new File(System.getProperty("user.dir")
+				+ "//map//galilei.txt");
+		return ConcreteMapFactory.createMap(fileMap);
 
 	}
-
+	
+	Map (Sector[][] grid, String name){
+		this.grid=grid;
+		this.name=name;
+	}
 }
--- cerridifebbo/src/main/java/it/polimi/ingsw/cerridifebbo/model/Sector.java
+++ cerridifebbo/src/main/java/it/polimi/ingsw/cerridifebbo/model/Sector.java
@@ -5,8 +5,6 @@ import java.util.ArrayList;
 public abstract class Sector {
 	private Coordinate coordinate;
 	private boolean passable;
-	private ArrayList<Player> playerInside;
-
 	private Sector north;
 	private Sector south;
 	private Sector northEast;
@@ -43,18 +41,6 @@ public abstract class Sector {
 		this.south = sector;
 	}
 
-	public void playerEnters(Player player) {
-		playerInside.add(player);
-	}
-
-	public void playerLeaves(Player player) {
-		playerInside.remove(player);
-	}
-
-	public boolean containsPlayer(Player player) {
-		return playerInside.contains(player);
-	}
-
 	public boolean isPassable() {
 		return passable;
 	}
@@ -68,37 +54,40 @@ public abstract class Sector {
 			return false;
 		}
 		int maxMovement = p.getMaxMovement();
-		Sector playerSector = p.getSector();
-
-		return recursive(playerSector, maxMovement);
-
-	}
-
-	private boolean recursive(Sector playerSector, int maxMovement) {
-		if (playerSector == null || maxMovement < 0
-				|| !playerSector.isPassable())
-			return false;
-		if (playerSector.equals(this))
-			return true;
-
-		for (Sector sector : playerSector.getAdjacentSector()) {
-			if (recursive(sector, maxMovement--)) {
-				return true;
-			}
-		}
-		return false;
-
-	}
-
-	public ArrayList<Sector> getAdjacentSector() {
 		ArrayList<Sector> sectorList = new ArrayList<Sector>();
-		sectorList.add(north);
-		sectorList.add(northEast);
-		sectorList.add(northWest);
-		sectorList.add(south);
-		sectorList.add(southEast);
-		sectorList.add(southWest);
-		return sectorList;
+		sectorList.add(p.getPos());
+		for (; maxMovement > 0; maxMovement--) {
+			addAdjacentSector(sectorList);
+			for (Sector s : sectorList) {
+				if (s.toString().equals(toString()))
+					return true;
+			}
+			System.out.println("sector list : " + sectorList);
+		}
+
+		return false;
+	}
+
+	private void addAdjacentSector(ArrayList<Sector> sectorList) {
+		ArrayList<Sector> temp = new ArrayList<Sector>();
+		for (Sector s : sectorList) {
+			if (s.north != null && s.north.isPassable())
+				temp.add(s.north);
+			if (s.northEast != null && s.northEast.isPassable())
+				temp.add(s.northEast);
+			if (s.northWest != null && s.northWest.isPassable())
+				temp.add(s.northWest);
+			if (s.south != null && s.south.isPassable())
+				temp.add(s.south);
+			if (s.southEast != null && s.southEast.isPassable())
+				temp.add(s.southEast);
+			if (s.southWest != null && s.southWest.isPassable())
+				temp.add(s.southWest);
+		}
+		for (Sector s : temp) {
+			if (!sectorList.contains(s))
+				sectorList.add(s);
+		}
 	}
 
 	@Override
