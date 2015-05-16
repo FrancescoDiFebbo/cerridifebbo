4bff3357c29aee4b6ad59265c0cd0a1596eb0032
StefanoCerri <stefano1.cerri@mail.polimi.it>
2015-05-16 11:55:52 +0200

    #modifiche turn e prova di un test

--- cerridifebbo/src/test/java/it/polimi/ingsw/cerridifebbo/MapTest.java
+++ cerridifebbo/src/test/java/it/polimi/ingsw/cerridifebbo/MapTest.java
@@ -9,11 +9,14 @@ import org.junit.Test;
 public class MapTest {
 
 	@Test
-	public void testReachableSectors(){
+	public void testReachableSectors() {
 		Map map = Map.getInstance();
 		Sector start = map.getCell(5, 5);
 		Sector end = map.getCell(7, 9);
 		int maxMovement = 4;
 		assertTrue(start.getReachableSectors(maxMovement).contains(end));
+		start = map.getCell(5, 5);
+		end = map.getCell(10, 11);
+		assertFalse(start.getReachableSectors(maxMovement).contains(end));
 	}
 }
